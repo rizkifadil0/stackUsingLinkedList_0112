6466a3e566b718e9028f7254ae1b95a1e020576e
rixkifadil@gmail.com <rixkifadil@gmail.com>
2025-06-05 15:17:41 +0700

    add pop operation

--- stack.cpp
+++ stack.cpp
@@ -41,3 +41,6 @@ class Stack
     {
         return top == NULL; //Return true if the top pointer is null, indicating an empty stack
     }
+
+    //pop operation: Remove the topmost element from the stack
+    void pop()
